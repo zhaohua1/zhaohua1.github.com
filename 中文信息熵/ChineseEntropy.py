55230a5544450e13b0a906fdb359cc4892b75c5d
zhaohua1 <936965866@qq.com>
2023-03-18 22:59:06 +0800

    中文信息熵计算代码和报告

--- 中文信息熵/ChineseEntropy.py
+++ 中文信息熵/ChineseEntropy.py
@@ -8,6 +8,9 @@ import configparser
 import requests
 from bs4 import BeautifulSoup
 
+
+import matplotlib.pyplot as plt
+
 #读取txt和url,组合到outfile=allChinese.txt中去
 stratTime=time.time()
 def read_data(path):
@@ -44,6 +47,15 @@ stopTxt=open(stoppath,'r',encoding='utf-8').read()
 path='E:\\课程\\2023春季\\自然语言处理\\jyxstxtqj_downcc.com'
 files,allTextPath=read_data(path)
 
+def countleader(ChineseCounts,n):
+    dictCountLeader={}  # 计算以前n-1个词为首的n元组的总个数
+    for item in ChineseCounts.items():
+        word, count = item
+        if n>=2:
+           dictCountLeader[word[0:n-1]]=dictCountLeader.get(word[0:n-1],0)+count
+        else:
+            dictCountLeader=ChineseCounts
+    return dictCountLeader
 #计算n-gram信息熵
 
 def getEntropy(text,n,stopTxt):
@@ -55,16 +67,42 @@ def getEntropy(text,n,stopTxt):
         pair=tuple(pair)
         ChineseCounts[pair]=ChineseCounts.get(pair,0)+1
     #计算熵
+    #获取n元组总个数
     wordLen=sum(ChineseCounts.values())
+    # 获取前n-1个词为首的n元组的总个数
+
+    dictCountLeader = countleader(ChineseCounts, n)
+    #如果是1元模型,则dictCountLeader=ChineseCounts
     entropy=0
     for item in ChineseCounts.items():
         word,count=item
-        entropy-=(count/wordLen)*math.log((count/wordLen),2)
+        # 联合概率每个n元词组在语料库中出现的频率
+        probilityUnion = count / wordLen
+        # 获取条件概率可近似等于每个n元词组在
+        # 语料库中出现的频率与以该n元词组的前n-1词为词首的二元词组的频数的比值。
+        probilityCondition=probilityUnion
+        if n>=2:
+            probilityCondition = count/dictCountLeader[word[0:n-1]]
+        entropy-=probilityUnion*math.log(probilityCondition,2)
     return entropy
 
-allText=open(allTextPath,'r',encoding='ANSI').read()#读取是个字符串
-entropy=getEntropy(allText,1,stopTxt)
+allText=open(allTextPath,'r',encoding='ANSI').read()#读取成为字符串
+entropy=getEntropy(allText,3,stopTxt)
 print(entropy)
 print('总耗时{:.1f}s'.format(time.time()-stratTime))
 
 
+# entropy=[]
+# for i in range(1,6):
+#     entropyValue=getEntropy(allText,i,stopTxt)
+#     entropy.append(i)
+#
+# print(entropy)
+# print('总耗时{:.1f}s'.format(time.time()-stratTime))
+# X=[]
+# for i in range(5):
+#     X.append(str(i+1)+'-gram')
+# plt.bar(X,entropy)
+# plt.xlabel('n-gram')
+# plt.ylabel('entropy value')
+# plt.show()
